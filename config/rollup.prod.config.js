44947bfa168b11fb8a0ba2c42e26c4e5d1066c3e
Job <Manduro@users.noreply.github.com>
2016-09-29 17:10:57 +0200

    fix(rollup): do not generate source map for prod

--- config/rollup.prod.config.js
+++ config/rollup.prod.config.js
@@ -3,4 +3,6 @@ const rollupConfig = require('./rollup.config');
 // https://github.com/rollup/rollup/wiki/JavaScript-API
 
 rollupConfig.entry = '.tmp/app/main.prod.js';
+rollupConfig.sourceMap = false;
+
 module.exports = rollupConfig;
