b0a55b37474e8b15c4e510a3dc7932f61e4927e0
Justin Willis <jgw9617@gmail.com>
2016-09-30 09:58:54 -0500

    fix(config): fixes #48

--- config/rollup.prod.config.js
+++ config/rollup.prod.config.js
@@ -1,6 +1,47 @@
-const rollupConfig = require('./rollup.config');
+var ngTemplate = require('../dist/plugins/ng-template').ngTemplate;
+var nodeResolve = require('rollup-plugin-node-resolve');
+var commonjs = require('rollup-plugin-commonjs');
 
 // https://github.com/rollup/rollup/wiki/JavaScript-API
 
-rollupConfig.entry = '.tmp/app/main.prod.js';
-module.exports = rollupConfig;
+module.exports = {
+  /**
+   * entry: The bundle's starting point. This file will
+   * be included, along with the minimum necessary code
+   * from its dependencies
+   */
+  entry: '.tmp/app/main.prod.js',
+
+  /**
+   * sourceMap: If true, a separate sourcemap file will
+   * be created.
+   */
+  sourceMap: true,
+
+  /**
+   * format: The format of the generated bundle
+   */
+  format: 'iife',
+
+  /**
+   * dest: the output filename for the bundle in the buildDir
+   */
+  dest: 'main.js',
+
+  /**
+   * plugins: Array of plugin objects, or a single plugin object.
+   * See https://github.com/rollup/rollup/wiki/Plugins for more info.
+   */
+  plugins: [
+    ngTemplate(),
+    commonjs(),
+    nodeResolve({
+      module: true,
+      jsnext: true,
+      main: true,
+      browser: true,
+      extensions: ['.js']
+    })
+  ]
+
+};
